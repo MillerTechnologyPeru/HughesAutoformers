76d29bfeddc281720f945000b719a164a4c124fd
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 15:55:42 -0500

    Add `AccessoryManager.log()`

---
+++ HughesAutoformers.swiftpm/Model/AccessoryManagerLog.swift
@@ -0,0 +1,15 @@
+//
+//  AccessoryManagerLog.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 3/9/24.
+//
+
+import Foundation
+
+public extension AccessoryManager {
+    
+    func log(_ message: String) {
+        print(message)
+    }
+}
