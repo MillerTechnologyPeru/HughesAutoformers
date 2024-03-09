db61d833b5a39280b23a695ef9b37c82daa2cd82
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-09 15:56:31 -0500

    Add `ContentView`

---
+++ HughesAutoformers.swiftpm/Views/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import HughesAutoformers
+
+struct ContentView: View {
+    
+    @EnvironmentObject
+    var store: AccessoryManager
+    
+    var body: some View {
+        NavigationView {
+            NearbyDevicesView()
+        }
+    }
+}
